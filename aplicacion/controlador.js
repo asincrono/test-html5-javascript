4965dba98387d2f5f02034e8419a071d89f9c945
Manuel C. Piñeiro Mourazos <asincrono@gmail.com>
2014-03-25 02:25:49 +0100

    Funciona!

--- aplicacion/controlador.js
+++ aplicacion/controlador.js
@@ -7,17 +7,86 @@ var getTime = function () {
     return (new Date()).getTime();
 };
 
-var Task = function (content, time) {
+var Task = function (content) {
     'use strict';
     this.content = content;
-    this.time = time;
+    this.time = getTime();
 };
 
 Task.prototype.toString = function () {
     'use strict';
-    return 'c:' + this.content + 't:' + this.time;
+    return 'Tarea: ' + this.content + ' (creada a las: ' + (new Date(this.time)).toLocaleString() + ')';
 };
 
+Task.toString = function () {
+    'use strict';
+    return 'Tarea: ' + this.content + ' (creada a las: ' + (new Date(this.time)).toLocaleString() + ')';
+};
 
+var getTaskArray = function () {
+    'use strict';
+    var tasksSer = localStorage.getItem('task');
+    if (tasksSer) {
+        return JSON.parse(tasksSer);
+    } else {
+        return [];
+    }
+};
 
-console.log(getTime());
+var showList = function (taskArray) {
+    'use strict';
+    var i, listItem, numTasks;
+    if (taskArray) {
+        for (i = 0; i < taskArray.length; i += 1) {
+            listItem = document.createElement('li');
+            listItem.innerHTML = taskArray[i].toString();
+            listTask.appendChild(listItem);
+        }
+    }
+};
+
+var addListItem = function (task) {
+    'use strict';
+    var listItem = document.createElement('li');
+    listItem.innerHTML = task.toString();
+    listTask.appendChild(listItem);
+};
+
+var saveTask = function (task) {
+    'use strict';
+    var taskArray = getTaskArray();
+
+    if (!taskArray) {
+        taskArray = [];
+    }
+
+    taskArray.push(task);
+
+    localStorage.setItem('task', JSON.stringify(taskArray));
+};
+
+var addTask = function (content) {
+    'use strict';
+    var task, taskArray;
+
+    task = new Task(content);
+    saveTask(task);
+    addListItem(task);
+};
+
+var initialize = function () {
+    'use strict';
+
+    var taskArray = getTaskArray();
+    showList(taskArray);
+
+    // Activar botón.
+    bttSaveTask.addEventListener('click', function () {
+        var content = inputTask.value;
+        if (content) {
+            addTask(content);
+        }
+    });
+};
+
+initialize();
